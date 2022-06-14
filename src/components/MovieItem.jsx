affc44ce71a75cf56080132176327290a0b614da
drift2000 <drift2000@gmail.com>
2022-06-15 01:57:39 +0300

    h/w: change button fn

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -10,6 +10,12 @@ class MovieItem extends React.Component {
     };
   }
 
+  willWatchButton = () => {
+    this.setState({
+      willWatch: !this.state.willWatch
+    })
+  }
+
   render() {
     const { movie, removeMovie, addMovieToWillWatch, removeMovieFromWillWatch } = this.props;
     return (
@@ -24,7 +30,19 @@ class MovieItem extends React.Component {
           <h6 className="card-title">{movie.title}</h6>
           <div className="d-flex justify-content-between align-items-center">
             <p className="mb-0">Rating: {movie.vote_average}</p>
-            {this.state.willWatch ? (
+
+            <button
+              type="button"
+              onClick={() => {
+                this.willWatchButton();
+                this.state.willWatch ? removeMovieFromWillWatch(movie) : addMovieToWillWatch(movie)
+              }}
+              className={this.state.willWatch ? "btn btn-success" : "btn btn-secondary"}
+            >
+              {this.state.willWatch ? "Remove Will Watch" : "Add Will Watch"}
+            </button>
+
+            {/* {this.state.willWatch ? (
               <button
                 type="button"
                 className="btn btn-success"
@@ -49,8 +67,7 @@ class MovieItem extends React.Component {
               >
                 Add Will Watch
               </button>
-            )}
-
+            )} */}
           </div>
           <button type="button" onClick={removeMovie.bind(null, movie)}>
             Delete
